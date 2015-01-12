507006fc7ac4a457357a2e7c8e97d5f617d48630
Matt Linaberry <matt.linaberry@we-ics.com>
2015-01-11 21:21:27 -0500

    Initial Push
    Completed the first Shoe Size Converter lesson.

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextfield: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = mensShoeSizeTextfield.text
+        let numberFromTextField = sizeFromTextField.toInt()
+        var integerFromTextField = numberFromTextField!  // 'unwrap' the optional
+        let conversionConstant = 30
+        integerFromTextField += conversionConstant
+        mensConvertedShoeSizeLabel.hidden = false
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"  //like printf/etc...
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
